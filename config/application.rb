968e75eebf33d2f51747bf0071d9c5ea84859b31
Marc Dagatan <marc.dagatan@gmail.com>
2019-02-05 10:34:47 +0800

    configure rack cors

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,16 @@ module RailsApiBoilerplate
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 expose: ['access-token', 'expiry', 'token-type', 'uid',
+                          'client'],
+                 methods: %i[get post options delete put patch head]
+      end
+    end
   end
 end
