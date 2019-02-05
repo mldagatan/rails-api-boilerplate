d1062a7f3706b22ba5cc365d6833dc48d58efed3
Marc Dagatan <marc.dagatan@gmail.com>
2019-02-05 12:03:20 +0800

    user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', controllers: {
-    registrations: 'api/v1/registrations'
+    registrations: 'api/v1/registrations',
+    sessions: 'api/v1/sessions'
   }
 end
