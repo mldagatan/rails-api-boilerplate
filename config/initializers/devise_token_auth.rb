e4ed643b1309b35ec76e9fc7c647c25db75b3e6c
Marc Dagatan <marc.dagatan@gmail.com>
2019-02-05 12:33:01 +0800

    changes to devise token auth configs

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,19 +1,11 @@
 # frozen_string_literal: true
 
 DeviseTokenAuth.setup do |config|
-  # By default the authorization headers will change after each request. The
-  # client is responsible for keeping track of the changing tokens. Change
-  # this to false to prevent the Authorization header from changing after
-  # each request.
-  # config.change_headers_on_each_request = true
-
-  # By default, users will need to re-authenticate after 2 weeks. This setting
-  # determines how long tokens will remain valid after they are issued.
-  # config.token_lifespan = 2.weeks
-
-  # Sets the max number of concurrent devices per user, which is 10 by default.
-  # After this limit is reached, the oldest tokens will be removed.
-  # config.max_number_of_devices = 10
+  config.change_headers_on_each_request = false
+  config.token_lifespan = 6.months
+  config.max_number_of_devices = 10
+  config.remove_tokens_after_password_reset = true
+  config.default_confirm_success_url = '/'
 
   # Sometimes it's necessary to make several requests to the API at the same
   # time. In this case, each request in the batch will need to share the same
@@ -47,5 +39,4 @@ DeviseTokenAuth.setup do |config|
   # If, however, you wish to integrate with legacy Devise authentication, you
   # can do so by enabling this flag. NOTE: This feature is highly experimental!
   # config.enable_standard_devise_support = false
-  config.default_confirm_success_url = '/'
 end
